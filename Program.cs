463a1fdd7b9a6832049561d1e2dbf6ddc2bfbb93
WIN_NT\x16j051 <x16j051@chiba-fjb.ac.jp>
2017-05-12 11:41:44 +0900

    初回

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            double pai;
+            string name;
+
+            score = (int)0.5;
+            score2 = 3;
+            pai = 3.1415;
+            name = "TARO";
+
+            Console.WriteLine("SCORE={0}", score);
+            Console.WriteLine("AVE={0}", pai);
+            Console.WriteLine("NAME={0}", name);
+        }
+    }
+}
